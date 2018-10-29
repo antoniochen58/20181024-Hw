3a37d0e580579f4efa928a0af7a62a4b81662927
antoniochen58 <43198397+antoniochen58@users.noreply.github.com>
2018-10-29 22:39:04 +0800

    20181024 hw

---
+++ Main.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        //月份轉換
+//        Scanner scn=new Scanner(System.in);
+//        int a = scn.nextInt();
+//        switch (a) {
+//            case 1:
+//                System.out.println("Janunary");
+//                break;
+//            case 2:
+//                System.out.println("February");
+//                break;
+//            case 3:
+//                System.out.println("March");
+//                break;
+//            case 4:
+//                System.out.println("Apirl");
+//                break;
+//            case 5:
+//                System.out.println("May");
+//                break;
+//            case 6:
+//                System.out.println("June");
+//                break;
+//            case 7:
+//                System.out.println("July");
+//                break;
+//            case 8:
+//                System.out.println("August");
+//                break;
+//            case 9:
+//                System.out.println("September");
+//                break;
+//            case 10:
+//                System.out.println("October");
+//                break;
+//            case 11:
+//                System.out.println("November");
+//                break;
+//            case 12:
+//                System.out.println("December");
+//                break;}
+
+//        //季節判斷
+//        Scanner scn=new Scanner(System.in);
+//        String str=scn.next();
+//
+//        switch(str){
+//            case "Spring":
+//                System.out.println("2~4月");
+//            case "Summer":
+//                System.out.println("5~7月");
+//            case "Fall":
+//                System.out.println("8~11月");
+//            case "Winter":
+//                System.out.println("12~1月");
+
+        //奇數相加
+//        Scanner scn=new Scanner(System.in);
+//        int a=scn.nextInt();
+//        int b=scn.nextInt();
+//        int result=0;
+//        for (int i=a;i<=b;i++){
+//            if (i % 2 != 0)
+//            result=result+i;}
+//        System.out.println(result);
+
+        //偶數相加
+//        Scanner scn=new Scanner(System.in);
+//        int a=scn.nextInt();
+//        int b=scn.nextInt();
+//        int result=0;
+//        for (int i=a;i<=b;i++){
+//            if (i % 2 == 0)
+//                result=result+i;}
+//        System.out.println(result);
+
+    }
+
+
+
+}
+
